928c330c7bf298b9a3b83f87e3851c0bf64ce909
Haruka Teduka <th_kiyose@outlook.com>
2020-07-03 20:41:32 +0900

    aws

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -2,8 +2,8 @@ $worker  = 2
 $timeout = 30
 #自分のアプリケーション名（currentがつくことに注意）
 $app_dir = "/var/www/taskmanager/current"
-$listen  = File.expand_path 'tmp/sockets/unicorn.sock', $app_dir
-$pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
+$listen  = File.expand_path 'var/sockets/unicorn.sock', $app_dir
+$pid     = File.expand_path 'var/run/unicorn.pid', $app_dir
 $std_log = File.expand_path 'log/unicorn.log', $app_dir
 # 上記で設定したものが適応されるよう定義
 worker_processes  $worker
